45586b6cc60b2f9e80805327a6b87d87d9e819c3
Ethan Bensman <ebenz99@gmail.com>
2020-12-14 14:24:46 -0500

    fixed api

--- src/utilities/getMove.js
+++ src/utilities/getMove.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { BACKEND_ENDPOINT } from '../constants.js';
 
 const getMove = () => {
-    return axios({method: 'get', url: `${BACKEND_ENDPOINT}/nextmove`,
+    return axios({method: 'get', url: `${BACKEND_ENDPOINT}nextmove`,
         params: {
-            'board': "Test"
+            'board': "hi"
         }
     })
     .then((response) => {
